5474614dc47c4fae104db956d57e224c3a1adecc
fathoni <fathonizikri11@gmail.com>
2024-02-17 17:03:53 +0700

    --

--- messages/dist/messages/messages.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { CreateMessageDto } from './dtos/create-message.dto';
-import { MessagesService } from './messages.service';
-export declare class MessagesController {
-    messagesService: MessagesService;
-    constructor(messagesService: MessagesService);
-    listMessages(): Promise<any>;
-    createMessage(body: CreateMessageDto): Promise<any>;
-    getMessage(id: string): Promise<any>;
-}
